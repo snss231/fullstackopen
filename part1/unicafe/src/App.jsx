62537fd1d9e978dace698fa039e209e8909436b4
Sean Ng <sean.ng@xpf.com>
2025-09-17 14:19:18 +0800

    1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,6 +2,22 @@ import { useState } from 'react'
 
 const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
 
+const Statistics = ({good, bad, neutral}) => {
+  const total = good + bad + neutral
+
+  return (
+    <div>
+      <h1>statistics</h1>
+      <Statistic label="good" value={good}></Statistic>
+      <Statistic label="neutral" value={neutral}></Statistic>
+      <Statistic label="bad" value={bad}></Statistic>
+      <Statistic label="all" value={total}></Statistic>
+      <Statistic label="average" value={(good - bad) / total}></Statistic>
+      <Statistic label="positive" value={good / total * 100 + "%"}></Statistic>
+    </div>
+  )
+}
+
 const Statistic = ({label, value}) => <div>{label} {value}</div>
 
 const App = () => {
@@ -10,21 +26,13 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const total = good + bad + neutral
-
   return (
     <div>
       <h1>give feedback</h1>
       <Button text="good" onClick={ () => setGood(good + 1) }></Button>
       <Button text="neutral" onClick={ () => setNeutral(neutral + 1) }></Button>
       <Button text="bad" onClick={ () => setBad(bad + 1) }></Button>
-      <h1>statistics</h1>
-      <Statistic label="good" value={good}></Statistic>
-      <Statistic label="neutral" value={neutral}></Statistic>
-      <Statistic label="bad" value={bad}></Statistic>
-      <Statistic label="all" value={total}></Statistic>
-      <Statistic label="average" value={(good - bad) / total}></Statistic>
-      <Statistic label="positive" value={good / total * 100 + "%"}></Statistic>
+      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
     </div>
   )
 }
