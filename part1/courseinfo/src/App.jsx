bbb06ba8fae7bd39d148fab2a1d6d06d6a4a4adb
Sean Ng <sean.ng@xpf.com>
2025-09-17 10:30:08 +0800

    Add courseinfo exercise

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,41 @@
+const Header = (props) => {
+    return (
+        <h1>{props.course}</h1>
+    )
+}
+
+const Part = (props) => {
+    return (
+        <p>
+            {props.part} {props.exercise}
+        </p>
+    )
+}
+
+const Total = (props) => {
+    return (
+      <p>Number of exercises {props.total}</p>
+    )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course}></Header>
+      <Part part={part1} exercise={exercises1}></Part>
+      <Part part={part2} exercise={exercises2}></Part>
+      <Part part={part3} exercise={exercises3}></Part>
+      <Total total={exercises1 + exercises2 + exercises3}></Total>
+    </div>
+  )
+}
+
+export default App
