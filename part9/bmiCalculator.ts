f2dfa083d52f774f8c2fdc081a24d073d87e2758
Sean Ng <sean.ng@xpf.com>
2025-10-13 16:28:03 +0800

    9.1: Implement bmi calc

---
+++ part9/bmiCalculator.ts
@@ -0,0 +1,33 @@
+interface BmiValues {
+  height: number;
+  weight: number;
+}
+
+const parseArguments = (args: string[]): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+const calculateBmi = (height: number, weight: number) => {
+    console.log("Normal range")
+}
+
+try {
+  const { height, weight } = parseArguments(process.argv);
+  calculateBmi(height, weight);
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
