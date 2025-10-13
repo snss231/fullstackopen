8bce302633cfbf2b7d54c81a337148e8fb0105a1
Sean Ng <sean.ng@xpf.com>
2025-10-13 17:44:28 +0800

    9.5: Implement WebBMI

--- part9/bmiCalculator.ts
+++ part9/bmiCalculator.ts
@@ -1,33 +1,38 @@
 interface BmiValues {
-  height: number;
-  weight: number;
+    height: number;
+    weight: number;
 }
 
 const parseArguments = (args: string[]): BmiValues => {
-  if (args.length < 4) throw new Error('Not enough arguments');
-  if (args.length > 4) throw new Error('Too many arguments');
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
 
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-    return {
-      height: Number(args[2]),
-      weight: Number(args[3])
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            height: Number(args[2]),
+            weight: Number(args[3])
+        }
+    } else {
+        throw new Error('Provided values were not numbers!');
     }
-  } else {
-    throw new Error('Provided values were not numbers!');
-  }
 }
 
-const calculateBmi = (height: number, weight: number) => {
-    console.log("Normal range")
+const calculateBmi = (_height: number, _weight: number) => {
+    return "Normal range";
 }
 
-try {
-  const { height, weight } = parseArguments(process.argv);
-  calculateBmi(height, weight);
-} catch (error: unknown) {
-  let errorMessage = 'Something bad happened.'
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message;
-  }
-  console.log(errorMessage);
+if (require.main === module) {
+    try {
+        const { height, weight } = parseArguments(process.argv);
+        calculateBmi(height, weight);
+    } catch (error: unknown) {
+        let errorMessage = 'Something bad happened.'
+        if (error instanceof Error) {
+            errorMessage += ' Error: ' + error.message;
+        }
+        console.log(errorMessage);
+    }
 }
+
+
+export { calculateBmi };
--- part9/index.ts
+++ part9/index.ts
@@ -1,9 +1,29 @@
 import express from 'express';
 
+import { calculateBmi } from './bmiCalculator';
+
 const app = express();
 
 app.get('/hello', (_, res) => {
     res.send("Hello Full Stack!")
+});
+
+app.get('/bmi', (req, res) => {
+    const weight = Number(req.query.weight);
+    const height = Number(req.query.height);
+
+    if (Number.isNaN(weight) || Number.isNaN(height)) {
+        res.status(400).send({
+            error: "malformatted parameters"
+        });
+        return;
+    }
+
+    res.send({
+        weight: weight,
+        height: height,
+        bmi: calculateBmi(height, weight)
+    })
 });
 
 const PORT = 3003;
