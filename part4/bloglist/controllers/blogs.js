4a58a254ed5406f6feb9172e722f4f6b86f7de40
Sean Ng <sean.ng@xpf.com>
2025-11-26 17:08:18 +0800

    4.19: Blog List Expansion, step 7

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 
 const Blog = require('../models/blog')
 const User = require('../models/user')
@@ -9,10 +10,30 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+const extractTokenFromRequest = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+
+}
+
 blogsRouter.post('/', async (request, response) => {
+  var token = jwt.verify(extractTokenFromRequest(request), process.env.SECRET) 
+  if (!token.id){
+    return response.status(401)
+  }
+
+  const user = await User.findById(token.id)
+
+  if (!user) {
+    return response.status(400).json({ error: 'UserId missing or not valid' })
+  }
+  
   const blog = new Blog({
     ...request.body,
-    creator: (await User.findOne())._id
+    creator: user._id
   })
 
   const result = await blog.save()
