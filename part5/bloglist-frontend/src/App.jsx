d1d8868bcff26664177fe07af17a7a700bc1b9b1
Sean Ng <sean.ng@xpf.com>
2025-11-28 10:47:33 +0800

    5.1: Blog List Frontend, step 1

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,22 +1,73 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState()
+
+  const loginForm = () => {
+    return (
+      <div>
+        <h1>log in to application</h1>
+        <form onSubmit={handleLogin}>
+          <div>
+            <label>
+              username
+              <input type="text" value={username} onChange={({ target }) => setUsername(target.value)} />
+            </label>
+          </div>
+          <div>
+            <label>
+              password
+              <input type="text" value={password} onChange={({ target }) => setPassword(target.value)} />
+            </label>
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    )
+  }
+
+  const blogList = () => {
+    return (
+      <>
+        <h2>blogs</h2>
+        <p>{user.name} logged in</p>
+        <div>
+          {blogs.map(blog =>
+            <Blog key={blog.id} blog={blog} />)}
+        </div>
+      </>
+    )
+  }
+
+  const handleLogin = async e => {
+    e.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (err) {
+      alert(JSON.stringify(err))
+    }
+  }
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {!user && loginForm()}
+      {user && blogList()}
     </div>
   )
 }
